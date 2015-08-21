befa629959294b015ecad2ae3573da8a7ce88504
Jon Sowman <jon@hexoc.com>
2015-08-21 09:50:29 +0100

    Separated out shell USB stuff

---
+++ firmware-basestation/usbserial.h
@@ -0,0 +1,24 @@
+/**
+ * This file is part of the ukhasnet dongle project. 
+ *
+ * @file usbserial.h
+ * @author Jon Sowman <jon+github@jonsowman.com>
+ * @copyright Jon Sowman 2015
+ *
+ * @addtogroup usbserial
+ * @{
+ */
+
+#ifndef __USBSERIAL_H__
+#define __USBSERIAL_H__
+
+#include "ch.h"
+#include "hal.h"
+
+THD_FUNCTION(UsbSerThread, arg);
+
+#endif /* __USBSERIAL_H__ */
+
+/**
+ * @}
+ */
